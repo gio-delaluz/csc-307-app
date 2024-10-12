19022336a7b76eaaa7175bfae5cdba0c2cbc36bd
Giovanni De La Luz <delaluz@calpoly.edu>
2024-10-11 23:15:58 -0700

    enable query by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -45,6 +45,9 @@ const findUserByName = (name) => {
       (user) => user["name"] === name
     );
   };
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
   
 // Setting up an endpoint w/ app.get
 app.get("/", (req, res) => {
@@ -61,6 +64,16 @@ app.get("/users", (req, res) => {
       res.send(users);
     }
 });
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
