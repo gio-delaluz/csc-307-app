32047e55f959eb0a0940e14a98d69f82760c70a4
Giovanni De La Luz <delaluz@calpoly.edu>
2024-10-11 23:29:07 -0700

    add new users to user list w/ POST

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -48,6 +48,11 @@ const findUserByName = (name) => {
 
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
   
 // Setting up an endpoint w/ app.get
 app.get("/", (req, res) => {
@@ -74,6 +79,12 @@ app.get("/users/:id", (req, res) => {
       res.send(result);
     }
   });
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
 
 app.listen(port, () => {
   console.log(
